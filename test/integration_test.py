a275c170dc16b7ee271aa5ab49f16baedbb89bbb
raditsoic <ruuoreo.o@gmail.com>
2024-12-20 23:17:36 +0700

    fix: integration test

--- test/integration_test.py
+++ test/integration_test.py
@@ -8,6 +8,7 @@ import os
 from PIL import Image
 from torchvision import transforms
 import json
+import re
 
 labels = {
     0: "cardboard",
@@ -18,26 +19,44 @@ labels = {
     5: "trash"
 }
 
+def get_latest_version(base_dir="models"):
+    model_dirs = [d for d in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, d))]
+    version_pattern = re.compile(r'model_(\d+\.\d+)')
+    
+    versions = []
+    for model_dir in model_dirs:
+        match = version_pattern.match(model_dir)
+        if match:
+            versions.append(match.group(1))
+    
+    if not versions:
+        raise ValueError(f"No valid model versions found in {base_dir}.")
+    
+    latest_version = sorted(versions, key=lambda v: [int(x) for x in v.split('.')])[-1]
+    return latest_version
 
 def load_model(version, model, base_dir="models"):
     model_dir = os.path.join(base_dir, f"model_{version}")
     model_path = os.path.join(model_dir, f"model_{version}.pth")
     config_path = os.path.join(model_dir, "config.json")
 
+    # Load Weights
     model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
     model.eval()
 
+    # Load Config
     with open(config_path, "r") as f:
         config = json.load(f)
 
     print(f"Loaded model version {version} with config: {config}")
     return model, config
 
-model = TrashnetModel(num_classes=6)  
+# Test only latest model ver
+base_model_dir = "models"
+latest_version = get_latest_version(base_model_dir)
 
-# Load Model
-model_version = "1.1" 
-model, config = load_model(model_version, model)
+model = TrashnetModel(num_classes=6)
+model, config = load_model(latest_version, model, base_model_dir)
 
 # Image preprocessing to match training
 transform = transforms.Compose([
@@ -46,15 +65,17 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-test_folder = "test/images" 
+# Dummy data for inference
+test_folder = "test/images"
 test_images = os.listdir(test_folder)
 
+# Inference on test images
 model.eval()
 for image_name in test_images:
     image_path = os.path.join(test_folder, image_name)
     image = Image.open(image_path).convert("RGB")  
 
-    # Preprocess
+    # Preprocess data
     input_tensor = transform(image).unsqueeze(0) 
 
     # Inference
